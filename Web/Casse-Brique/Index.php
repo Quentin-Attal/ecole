5feaf7c0422ee04573382dd2da64e5e10bd31132
Quentin <56029600+Quentin-Attal@users.noreply.github.com>
2019-12-05 23:51:10 +0100

    Bouger barre

--- Web/Casse-Brique/Index.php
+++ Web/Casse-Brique/Index.php
@@ -6,7 +6,7 @@ $page = new \Page\Page(basename(__FILE__));
 
 ?>
 
-<body class="bg-light">
+<body class="bg-dark">
 <header>
     <div class="navbar navbar-dark bg-dark shadow-sm">
         <div class="container d-flex justify-content-between">
@@ -26,7 +26,10 @@ $page = new \Page\Page(basename(__FILE__));
 
     <div class="container-fluid m-auto p-2">
         <div>
-            <table class="border-danger" onload="CreateTable();">
+            <table class="border border-primary table-bordered w-50 h-75 m-auto">
+                <tbody  id="table">
+
+                </tbody>
             </table>
         </div>
         <div></div>
@@ -36,7 +39,74 @@ $page = new \Page\Page(basename(__FILE__));
 </body>
 
 <script>
-    function CreateTable(){
-        console.log("hello");
+    window.onload = function(){
+        CreateTable(document.getElementById("table"));
+    };
+    function CreateTable(element){
+        let numbTr = 5;
+        let numbTd = 6;
+        for (let i = 0; i < numbTd; i++){
+            let obj = document.createElement("tr");
+            for (let j = 0; j < numbTr; j++){
+                let color;
+                switch (j) {
+                    case 0:
+                        color = "bg-primary";
+                        break;
+                    case 1:
+                        color = "bg-secondary";
+                        break;
+                    case 2:
+                        color = "bg-success";
+                        break;
+                    case 3:
+                        color = "bg-danger";
+                        break;
+                    case 4:
+                        color = "bg-warning";
+                        break;
+                    case 5:
+                        color = "bg-info";
+                        break;
+                }
+                let brique = document.createElement("td");
+                brique.className += Math.floor(Math.random() * Math.floor(2)) === 1 ? color : "border-0";
+                obj.appendChild(brique);
+            }
+            element.appendChild(obj);
+        }
+        for (let i = 0; i < numbTd-1; i++) {
+            let bas = document.createElement("tr");
+            let ghost = document.createElement("td");
+            ghost.className = "border-0";
+            bas.appendChild(ghost);
+            element.appendChild(bas);
+        }
+        let bas = document.createElement("tr");
+        let Barre = document.createElement("div");
+        Barre.className += "border-0 position-relative bg-light";
+        Barre.style += ";height: 100%; width: 66%;";
+        Barre.id = "barre";
+        bas.appendChild(Barre);
+        element.appendChild(bas);
     }
+    document.getElementById("table").parentNode.addEventListener("mousemove", function(event) {
+        Move(event);
+    });
+
+    function Move(Element) {
+        let obj = document.getElementById("table").parentNode;
+        let barre = document.getElementById("barre");
+        if ((Element.clientX - obj.offsetLeft - barre.offsetWidth / 2) >= 0 &&
+            obj.offsetWidth + obj.offsetLeft >= Element.clientX + barre.offsetWidth / 2 ) {
+            barre.style.left = (Element.clientX - obj.offsetLeft - barre.offsetWidth / 2) + 'px';
+        }
+        else if (obj.offsetWidth + obj.offsetLeft <= Element.clientX + barre.offsetWidth / 2){
+            barre.style.left = obj.offsetWidth - 2 - barre.offsetWidth + 'px';
+        }
+        else if ((Element.clientX - obj.offsetLeft - barre.offsetWidth / 2) <= 0){
+            barre.style.left = '0px';
+        }
+    }
+
 </script>
